9cf1b64894bed6f0ac0b3fdc24b3a92088f2df0e
Brandon Bench <benchlord@gmail.com>
2019-12-04 13:00:59 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 git clone https://github.com/BenchLord/config.git $HOME/config
-git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh && mv $HOME/config/custom-zsh.theme $HOME/.oh-my-zsh/custom/themes
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 mv $HOME/config/.vimrc $HOME
 mv $HOME/config/.zshrc $HOME
+echo "$(ls $HOME/config)"
+mv $HOME/config/custom.zsh-theme HOME/.oh-my-zsh/custom/themes
 # TODO: clone vim plugins
-source $HOME/.zshrcs
+source $HOME/.zshrc
