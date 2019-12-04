5bb7197d4e0295d2fd366d36511bd0d03caada84
Brandon Bench <benchlord@gmail.com>
2019-12-04 12:56:09 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 git clone https://github.com/BenchLord/config.git $HOME/config
-git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh &&
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh && mv $HOME/config/custom-zsh.theme $HOME/.oh-my-zsh/custom/themes
 mv $HOME/config/.vimrc $HOME
 mv $HOME/config/.zshrc $HOME
-# TODO: figure out why this isn't working
-mv $HOME/config/custom-zsh.theme $HOME/.oh-my-zsh/custom/themes
 # TODO: clone vim plugins
-source $HOME/.zshrc
+source $HOME/.zshrcs
