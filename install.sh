6007fd6dee5d5c13d49d4fd30d52229b6df8c895
Brandon Bench <benchlord@gmail.com>
2019-12-04 13:11:46 -0700

    remove debug echo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/BenchLord/config.git $HOME/config
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 mv $HOME/config/.vimrc $HOME
 mv $HOME/config/.zshrc $HOME
-echo "$(ls $HOME/config)"
 mv $HOME/config/custom.zsh-theme $HOME/.oh-my-zsh/custom/themes
 # TODO: clone vim plugins
-source $HOME/.zshrc && rl
+# TODO: fix source line
+source $HOME/.zshrc
