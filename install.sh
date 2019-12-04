cda4958ff7a779444277fae403d2d570a3210280
Brandon Bench <benchlord@gmail.com>
2019-12-04 12:46:09 -0700

    fix typo

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/BenchLord/config.git $HOME/config
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 mv $HOME/config/.vimrc $HOME
 mv $HOME/config/.zshrc $HOME
+# TODO: figure out why this isn't working
 mv $HOME/config/custom-zsh.theme $HOME/.oh-my-zsh/custom/themes
 # TODO: clone vim plugins
-source $HOME/.zshrcs
+source $HOME/.zshrc
