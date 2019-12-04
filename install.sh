238c427113088667e42971a2696b6cc5fe5eab17
Brandon Bench <benchlord@gmail.com>
2019-12-04 11:32:50 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 git clone https://github.com/BenchLord/config.git $HOME/config
-rm -f $HOME/.vimrc $HOME/.zshrc $HOME/.vim
-mv $HOME/config/.vim $HOME
-mv $HOME/config/.vimrc $HOME
-mv $HOME/config/.zshrc $HOME
-rm -rf $HOME/config
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-y
+> y
+> rm -f $HOME/.vimrc $HOME/.zshrc $HOME/.vim
+> mv $HOME/config/.vim $HOME
+> mv $HOME/config/.vimrc $HOME
+> mv $HOME/config/.zshrc $HOME
+> rm -rf $HOME/config
