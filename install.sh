e1f32481b64b02017d30a7ee946541642393541d
Brandon Bench <benchlord@gmail.com>
2019-12-04 13:06:33 -0700

    fix typo

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 mv $HOME/config/.vimrc $HOME
 mv $HOME/config/.zshrc $HOME
 echo "$(ls $HOME/config)"
-mv $HOME/config/custom.zsh-theme HOME/.oh-my-zsh/custom/themes
+mv $HOME/config/custom.zsh-theme $HOME/.oh-my-zsh/custom/themes
 # TODO: clone vim plugins
 source $HOME/.zshrc
