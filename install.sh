64fa1534c763a6c09bc747c37020ab8fee0d0a3c
Brandon Bench <benchlord@gmail.com>
2019-12-04 12:27:37 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 git clone https://github.com/BenchLord/config.git $HOME/config
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-> y
-> rm -f $HOME/.vimrc $HOME/.zshrc $HOME/.vim
-> mv $HOME/config/.vim $HOME
-> mv $HOME/config/.vimrc $HOME
-> mv $HOME/config/.zshrc $HOME
-> rm -rf $HOME/config
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
+mv $HOME/config/custom-zsh.theme $HOME/.oh-my-zsh/custom/themes
+echo 'done'
+# TODO: clone vim plugins
